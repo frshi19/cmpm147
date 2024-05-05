156b1556a5183affe5e842edb19d8a3a599aa2d5
Frank Shi <frshi@ucsc.edu>
2024-05-05 15:10:42 -0700

    import ex5 glitch code

---
+++ experiment5/js/my_design.js
@@ -0,0 +1,36 @@
+/* exported getInspirations, initDesign, renderDesign, mutateDesign */
+
+
+function getInspirations() {
+    return [
+      {
+        name: "Lunch atop a Skyscraper", 
+        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/lunch-on-a-skyscraper.jpg?v=1714798266994",
+        credit: "Lunch atop a Skyscraper, Charles Clyde Ebbets, 1932"
+      },
+      {
+        name: "Train Wreck", 
+        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/train-wreck.jpg?v=1714798264965",
+        credit: "Train Wreck At Monteparnasse, Levy & fils, 1895"
+      },
+      {
+        name: "Migrant mother", 
+        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/migrant-mother.jpg?v=1714778906791",
+        credit: "Migrant Mother near Nipomo, California, Dorothea Lange, 1936"
+      },
+      {
+        name: "Disaster Girl", 
+        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/girl-with-fire.jpg?v=1714778905663",
+        credit: "Four-year-old Zoë Roth, 2005"
+      },
+    ];
+  }
+  
+  function initDesign(inspiration) {
+    return {};
+  }
+  
+  function renderDesign(design, inspiration) {}
+  
+  function mutateDesign(design, inspiration, rate) {}
+  
