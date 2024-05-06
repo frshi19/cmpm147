ed1741b56d9af4e177a5cd509807b72f976d2730
Frank Shi <frshi@ucsc.edu>
2024-05-05 17:46:59 -0700

    fix canvas size

--- experiment5/js/my_design.js
+++ experiment5/js/my_design.js
@@ -1,36 +1,71 @@
-/* exported getInspirations, initDesign, renderDesign, mutateDesign */
+/* exported p4_inspirations, p4_initialize, p4_render, p4_mutate */
 
 
 function getInspirations() {
-    return [
-      {
-        name: "Lunch atop a Skyscraper", 
-        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/lunch-on-a-skyscraper.jpg?v=1714798266994",
-        credit: "Lunch atop a Skyscraper, Charles Clyde Ebbets, 1932"
-      },
-      {
-        name: "Train Wreck", 
-        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/train-wreck.jpg?v=1714798264965",
-        credit: "Train Wreck At Monteparnasse, Levy & fils, 1895"
-      },
-      {
-        name: "Migrant mother", 
-        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/migrant-mother.jpg?v=1714778906791",
-        credit: "Migrant Mother near Nipomo, California, Dorothea Lange, 1936"
-      },
-      {
-        name: "Disaster Girl", 
-        assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/girl-with-fire.jpg?v=1714778905663",
-        credit: "Four-year-old Zoë Roth, 2005"
-      },
-    ];
+  return [
+    {
+      name: "Lunch atop a Skyscraper", 
+      assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/lunch-on-a-skyscraper.jpg?v=1714798266994",
+      credit: "Lunch atop a Skyscraper, Charles Clyde Ebbets, 1932"
+    },
+    {
+      name: "Train Wreck", 
+      assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/train-wreck.jpg?v=1714798264965",
+      credit: "Train Wreck At Monteparnasse, Levy & fils, 1895"
+    },
+    {
+      name: "Migrant mother", 
+      assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/migrant-mother.jpg?v=1714778906791",
+      credit: "Migrant Mother near Nipomo, California, Dorothea Lange, 1936"
+    },
+    {
+      name: "Disaster Girl", 
+      assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/girl-with-fire.jpg?v=1714778905663",
+      credit: "Four-year-old Zoë Roth, 2005"
+    },
+  ];
+}
+
+function initDesign(inspiration) {
+  resizeCanvas(inspiration.image.width / 2, inspiration.image.height / 2);
+  
+  let design = {
+    bg: 128,
+    fg: []
   }
   
-  function initDesign(inspiration) {
-    return {};
+  for(let i = 0; i < 100; i++) {
+    design.fg.push({x: random(width),
+                    y: random(height),
+                    w: random(width/2),
+                    h: random(height/2),
+                    fill: random(255)})
   }
+  return design;
+}
+
+function renderDesign(design, inspiration) {
   
-  function renderDesign(design, inspiration) {}
-  
-  function mutateDesign(design, inspiration, rate) {}
-  
+  background(design.bg);
+  noStroke();
+  for(let box of design.fg) {
+    fill(box.fill, 128);
+    rect(box.x, box.y, box.w, box.h);
+  }
+}
+
+function mutateDesign(design, inspiration, rate) {
+  design.bg = mut(design.bg, 0, 255, rate);
+  for(let box of design.fg) {
+    box.fill = mut(box.fill, 0, 255, rate);
+    box.x = mut(box.x, 0, width, rate);
+    box.y = mut(box.y, 0, height, rate);
+    box.w = mut(box.w, 0, width/2, rate);
+    box.h = mut(box.h, 0, height/2, rate);
+  }
+}
+
+
+function mut(num, min, max, rate) {
+    return constrain(randomGaussian(num, (rate * (max - min)) / 10), min, max);
+}
